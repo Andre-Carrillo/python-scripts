93637423b6bef6e9aff52c6f6f56c64fe1caf704
andre-carrillo <carrillomonteroandre@gmail.com>
2022-01-30 11:17:21 -0500

    commited

--- coviddata.py
+++ coviddata.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
-data_ubication="C://Users\carri\Documents\VSFolder\p5-and-processing\covidsimulator\data//at_20373.json"
+data_ubication="C://Users\carri\Documents\VSFolder\p5-and-processing\covidsimulator\data//at_7426.json"
+
 with open(data_ubication) as f:
     data = json.load(f)
 data=data["data"]
@@ -9,6 +10,7 @@ data_x=[i["time"] for i in data]
 data_y_ill=[i["ill"] for i in data]
 data_y_healthy=[i["healthy"] for i in data]
 data_y_inmune=[i["inmune"] for i in data]
+data_y_dead=[i["dead"] for i in data]
 
-plt.stackplot(data_x, data_y_ill, data_y_healthy, data_y_inmune)
+plt.stackplot(data_x, data_y_ill, data_y_healthy, data_y_inmune, data_y_dead, colors=["#c81230", "#161991", "#18af21", "#5c5a58"])
 plt.show()
