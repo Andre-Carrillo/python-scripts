5c7f1dc053193b048dd0ff1e781d984bb87211c2
andre-carrillo <carrillomonteroandre@gmail.com>
2022-01-30 12:39:36 -0500

    deleted useless notebook

--- coviddata.py
+++ coviddata.py
@@ -1,6 +1,6 @@
 import json
 import matplotlib.pyplot as plt
-data_ubication="C://Users\carri\Documents\VSFolder\p5-and-processing\covidsimulator\data//at_7426.json"
+data_ubication="C://Users\carri\Documents\VSFolder\p5-and-processing\covidsimulator\data//at_5731.json"
 
 with open(data_ubication) as f:
     data = json.load(f)
@@ -10,7 +10,9 @@ data_x=[i["time"] for i in data]
 data_y_ill=[i["ill"] for i in data]
 data_y_healthy=[i["healthy"] for i in data]
 data_y_inmune=[i["inmune"] for i in data]
-data_y_dead=[i["dead"] for i in data]
+# data_y_dead=[i["dead"] for i in data]
 
-plt.stackplot(data_x, data_y_ill, data_y_healthy, data_y_inmune, data_y_dead, colors=["#c81230", "#161991", "#18af21", "#5c5a58"])
+# plt.stackplot(data_x, data_y_ill, data_y_healthy, data_y_inmune, data_y_dead, colors=["#c81230", "#161991", "#18af21", "#5c5a58"])
+plt.stackplot(data_x, data_y_ill, data_y_healthy, data_y_inmune, colors=["#c81230", "#161991", "#18af21", "#5c5a58"])
+
 plt.show()
